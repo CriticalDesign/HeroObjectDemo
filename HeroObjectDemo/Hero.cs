934b5e7ce35f2a492d5ab06e9cd384994e5a8aea
Aaron <Aaron@DESKTOP-9EN67C2>
2024-01-23 18:58:16 -0500

    Add project files.

---
+++ HeroObjectDemo/Hero.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroObjectDemo
+{
+    internal class Hero
+    {
+        //This is a blueprint for a Hero - that's what an object class is
+
+
+        //attributes go here - attributes define the state of our object
+        private int _x, _y;     //hero coordinates
+        private int _health;    //hero health
+        private float _speed;   //hero speed (if the hero actually moved)
+        private string _name;   //hero name
+        private string _weapon; //hero current weapon
+
+        //Constructor 1 - sets the default values for the hero attributes
+        public Hero()
+        {
+            _name = "Zag";
+            _x = 10;
+            _y = 10;
+            _speed = 5.0f;
+            _weapon = "sword";
+            _health = 100;
+        }
+
+        //Constructor 2 - this constructor passed in values to have a custom object
+        public Hero(string nameIn, int xIn, int yIn, float speedIn, string weaponIn, int healthIn)
+        {
+            //check the xIn, yIn, and speedIn values and make sure they make sense, change them
+            //if they don't make sense
+            if (xIn < 0 || xIn > 1280) xIn = 0;
+            _x = xIn;
+
+            if (yIn < 0 || yIn > 1024)
+                yIn = 0;
+            _y = yIn;
+
+            if (speedIn < 0 || speedIn > 100)
+                speedIn = 5.0f;
+            _speed = speedIn;
+
+            //these variables could use some checks - try for yourself?
+            _name = nameIn;
+            _weapon = weaponIn;
+            _health = healthIn;
+        }
+
+
+        //getter or accessor methods - these methods return the attribute values
+        //they are a way to know what values the hero is currently holding.
+
+        //notice they all start with public, then have a "return type" that matches the type
+        //of each attribute.
+        public string GetName() { return _name; }
+        public int GetX() { return _x; }
+        public int GetY() { return _y; }
+        public int GetHealth() { return _health; }
+        public float GetSpeed() { return _speed;}
+        public string GetWeapon() { return _weapon; }
+        public bool IsDead() { return  _health <= 0; }
+
+
+        //setters or mutators - these methods "change", or mutate our object. They are 
+        //kind of like the costructor, but we call them AFTER the object has been constructed.
+
+        //notice how each one has void as the return type and they DON'T have a return statement 
+        //inside. Each of these methods should have some checks to make sure the incoming values make
+        //sense.
+        public void SetX(int xIn)
+        {
+            _x = xIn;
+        }
+        public void SetY(int yIn)
+        {
+            _y = yIn;
+        }
+        public void TakeDamage(int damageIn)
+        {
+            _health -= damageIn;
+        }
+        public void Heal(int healthIn)
+        {
+            _health += healthIn;
+        }
+        public void ChangeWeapon(string weaponIn)
+        {
+            _weapon = weaponIn;
+        }
+        public void ChangeSpeed(float speedIn)
+        {
+            _speed = speedIn;
+        }
+
+        //helper methods don't change or read the object attributes, instead they simply help
+        //the object to some other job or behaviour.
+        public int DealDamage()
+        {
+            Random rng = new Random();
+            return rng.Next(4, 21);
+        }
+
+
+
+    }
+}
---
+++ HeroObjectDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HeroObjectDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiated hero objects - or specific objects that are instances of
+            //of the Hero blueprint class
+            Hero hero = new Hero("Zagreus", 20, 100, 7.5f, "bow", 300);  //hero
+            Hero villain = new Hero("Theseus", 10, 100, 7.5f, "magic", 500); //villain
+            
+            //a quick battle loop
+            while(!(villain.IsDead() || hero.IsDead()))  //this is a method in the hero class
+            {
+                //hero does damage first
+                int heroDamage = (int)(hero.DealDamage() * 1.75f);  //this is how much damage the hero does
+                villain.TakeDamage(heroDamage);  //send damage in to the villain
+                Console.WriteLine(villain.GetName() + "'s health: " + villain.GetHealth());
+
+                //villain does damage next
+                int villainDamage = villain.DealDamage();  //this is how much damage the hero does
+                hero.TakeDamage(villainDamage);  //send damage in to the villain
+                Console.WriteLine(hero.GetName() + "'s health: " + hero.GetHealth());
+            }
+
+            if (hero.IsDead())
+                Console.WriteLine(villain.GetName() + " wins the battle.");
+            else if (villain.IsDead())
+                Console.WriteLine(hero.GetName() + " wins the battle.");
+            else
+                Console.WriteLine("Well folks, it's a tie!");
+
+
+            //this stuff we did in class and I simply commented it out.
+            /*
+            zag.SetX(1000);
+            zag.SetY(500);
+            zag.TakeDamage(50);
+            zag.Heal(2);
+            zag.ChangeWeapon("dead fish");
+
+            Console.WriteLine(zag.GetX());
+            Console.WriteLine(zag.GetY());
+            Console.WriteLine(zag.GetSpeed());
+            Console.WriteLine(zag.GetHealth());
+            Console.WriteLine(zag.GetName());
+            Console.WriteLine(zag.GetWeapon());
+
+            Console.WriteLine("---------");
+
+            Console.WriteLine(meg.GetX());
+            Console.WriteLine(meg.GetY());
+            Console.WriteLine(meg.GetSpeed());
+            Console.WriteLine(meg.GetHealth());
+            Console.WriteLine(meg.GetName());
+            Console.WriteLine(meg.GetWeapon());
+
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
